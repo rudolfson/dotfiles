8e7e219e17e30be5114036392f65c114ea2e985c
Lars Heller <l.heller@tarent.de>
2023-12-08 11:44:53 +0100

    add aliases c/v for copy/paste

--- oh-my-zsh-custom/00_aliases.zsh
+++ oh-my-zsh-custom/00_aliases.zsh
@@ -2,5 +2,6 @@
 # Common aliases not belonging to a specific category
 #
 
+alias grepsrc='grep --exclude-dir={node_modules,bin,build,target,dist}'
 alias c='xclip -selection clipboard'
 alias v='xclip -o -selection clipboard'
