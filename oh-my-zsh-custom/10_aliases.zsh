25cb42e4f20fcbb5424b9d0f238c061a48ba2e07
Lars Heller <l.heller@tarent.de>
2025-03-21 15:23:35 +0100

    changes after installing on ubuntu

--- oh-my-zsh-custom/10_aliases.zsh
+++ oh-my-zsh-custom/10_aliases.zsh
@@ -5,3 +5,19 @@
 alias grepsrc='grep --exclude-dir={node_modules,bin,build,target,dist}'
 alias c='xclip -selection clipboard'
 alias v='xclip -o -selection clipboard'
+
+if ! whence bat > /dev/null
+then
+  if whence batcat > /dev/null
+  then
+    alias bat=batcat
+  fi
+fi
+
+if ! whence fd > /dev/null
+then
+  if whence fdfind > /dev/null
+  then
+    alias fd=fdfind
+  fi
+fi
