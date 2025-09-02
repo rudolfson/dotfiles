1c3179a65b1e6e6e302ec8e2f0669d5004aeb302
Lars Heller <lhe@posteo.de>
2025-09-02 12:05:01 +0200

    tweaks for xfce

--- oh-my-zsh-custom/10_darkmode.zsh
+++ oh-my-zsh-custom/10_darkmode.zsh
@@ -20,7 +20,9 @@ switchdm() {
 
 # query current mode
 _query_dark_mode() {
-    gsettings get org.gnome.desktop.interface color-scheme
+    local current=$(gsettings get org.gnome.desktop.interface color-scheme)
+    [[ -z $current ]] && current="prefer-dark"
+    echo $current
 }
 
 # adjust env variables, terminal colors, etc. according to current mode
@@ -30,13 +32,35 @@ _adjust_environment_to_dark_mode() {
         export BAT_THEME=gruvbox-dark
         export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=240
         kitty +kitten themes --reload-in=all "Gruvbox Dark"
-
+        if _is_desktop_environment xfce; then
+            gsettings set org.gnome.desktop.interface gtk-theme Matcha-dark-azul
+        fi
     } else {
         export BAT_THEME=gruvbox-light
         export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=250
         kitty +kitten themes --reload-in=all "Gnome Light"
+        if _is_desktop_environment xfce; then
+            gsettings set org.gnome.desktop.interface gtk-theme Matcha-light-azul
+        fi
     }
 }
+
+_is_desktop_environment() (
+    local de=${1:-}
+    local DEs=${XDG_CURRENT_DESKTOP:-}
+
+    # Shortcut: If de is empty, check if empty DEs
+    if [[ -z "$de" ]]; then if [[ "$DEs" ]]; then return; else return 1; fi; fi
+
+    # Lowercase both
+    de=${de:l}; DEs=${DEs:l}
+
+    # Check de against each DEs component
+    IFS=:; for DE in $DEs; do if [[ "$de" == "$DE" ]]; then return; fi; done
+
+    # Not found
+    return 1
+)
 
 # trigger adjustments on initial load
 _adjust_environment_to_dark_mode
