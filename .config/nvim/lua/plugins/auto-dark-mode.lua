8640b79fb0cd5c076743300ba98d705788850159
Lars Heller <lars.heller@rewe-group.com>
2026-02-05 13:50:22 +0100

    added auto dark mode switching to nvim

---
+++ .config/nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,15 @@
+--
+-- Configure auto dark mode
+-- https://github.com/f-person/auto-dark-mode.nvim
+--
+
+return {
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
